fc07ce604d054c3bcd1291a028efcb3101922f83
Rafael Noll <rafanoll2004@gmail.com>
2023-01-16 14:34:59 -0300

    OCP - Open/Closed Pinciple

---
+++ src/ocp/main.ts
@@ -0,0 +1,23 @@
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shoppingCart';
+import { FiftyPercentDiscount } from './classes/discount';
+
+const fiftyPercentDiscount = new FiftyPercentDiscount();
+const shoppingCart = new ShoppingCart(fiftyPercentDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Geladeira', 1000));
+shoppingCart.addItem(new Product('MacBook', 8000));
+shoppingCart.addItem(new Product('IPhone', 2000));
+
+console.log(order.orderStatus);
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+order.checkout();
+console.log(order.orderStatus);
